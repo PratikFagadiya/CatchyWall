e580af970b54c5cd433f17700af87985697f9532
Pratik Fagadiya <pratikfagadiya@gmail.com>
2022-01-28 16:35:34 +0530

    handle use profile click

--- app/src/main/java/com/pratik/catchywall/presentation/viewmodels/CollectionViewModel.kt
+++ app/src/main/java/com/pratik/catchywall/presentation/viewmodels/CollectionViewModel.kt
@@ -2,7 +2,6 @@ package com.pratik.catchywall.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.pratik.catchywall.data.remote.repository.CollectionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
