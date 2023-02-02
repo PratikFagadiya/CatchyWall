1529918961331fafc6ce167599473d1655b279c4
Pratik Fagadiya <pratikfagadiya@gmail.com>
2023-02-02 16:51:44 +0530

    -> Redesign UI

--- app/src/main/java/com/pratik/catchywall/utils/Utils.kt
+++ app/src/main/java/com/pratik/catchywall/utils/Utils.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.os.Environment
 import timber.log.Timber
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 object Utils {
@@ -16,19 +18,23 @@ object Utils {
         if (!url.startsWith("http")) {
             return false
         }
-        val name = "temp.mcaddon"
+
+        val dateFormat = SimpleDateFormat("yyyyMMdd'T'HHmmss")
+        val timeStamp: String = dateFormat.format(Date())
+        val imageFileName = "picture_$timeStamp.jpg"
+
         try {
             val file = File(Environment.getExternalStorageDirectory(), "Download")
             if (!file.exists()) {
                 file.mkdirs()
             }
-            val result = File((file.getAbsolutePath() + File.separator).toString() + name)
+            val result = File((file.absolutePath + File.separator) + imageFileName)
             val downloadManager =
                 context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager?
             val request = DownloadManager.Request(Uri.parse(url))
             request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
             request.setDestinationUri(Uri.fromFile(result))
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
             downloadManager?.enqueue(request)
             //mToast(mContext, "Starting download...");
             MediaScannerConnection.scanFile(
